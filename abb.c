c69c197dc13cb790bd8cfa14a0bfa3a402a2190e
Nicolas De Giacomo <nicolas.degiacomo@mercadolibre.com>
2017-05-09 11:21:39 -0300

    Done abb_t + abb_crear + abb_guardar

--- abb.c
+++ abb.c
@@ -1,12 +1,21 @@
 #include "abb.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 
 /* ******************************************************************
  *                DEFINICION DE LOS TIPOS DE DATOS
  * *****************************************************************/
 
+struct abb{
+    abb_comparar_clave_t comparador;
+    abb_destruir_dato_t destructor;
+    void* dato;
+    const char* clave;
+    abb_t* izq;
+    abb_t* der;
+};
 typedef struct abb abb_t;
 
 typedef int (*abb_comparar_clave_t) (const char *, const char *);
@@ -20,9 +29,56 @@ typedef void (*abb_destruir_dato_t) (void *);
  * *****************************************************************/
 
 
-abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato);
+abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato){
+    abb_t* arbol = malloc(sizeof(abb_t));
+    if (!arbol)
+        return NULL;
+    arbol->destructor = destruir_dato;
+    arbol->comparador = cmp;
+    arbol->dato = NULL;
+    arbol->clave = NULL;
+    arbol->der = NULL;
+    arbol->izq = NULL;
+    return arbol;
+}
 
-bool abb_guardar(abb_t *arbol, const char *clave, void *dato);
+bool abb_guardar(abb_t *arbol, const char *clave, void *dato){
+
+    if (!arbol->clave){
+        arbol->clave = malloc(sizeof(char) * (strlen(clave) + 1) );
+        if (!arbol->clave)
+            return false;
+
+        strcpy(arbol->clave, clave);
+        arbol->dato = dato;
+        return true;
+    }
+
+    //0 -> Iguales. <0 -> 1º mas grande. >0 2º mas grande.
+    if(!arbol->comparador(arbol->clave, clave)){
+        arbol->destructor(arbol->dato);
+        arbol->dato = dato;
+        return true;
+    }
+
+    if(arbol->comparador(arbol->clave, clave) < 0){
+        if (!arbol->izq){
+            arbol->izq = abb_crear(arbol->comparador, arbol->destructor);
+            if (!arbol->izq)
+                return false;
+        }
+        return abb_guardar(arbol->izq, clave, dato);
+    }
+    else{
+        if (!arbol->der){
+            arbol->der = abb_crear(arbol->comparador, arbol->destructor);
+            if (!arbol->der)
+                return false;
+        }
+        return abb_guardar(arbol->der, clave, dato);
+    }
+
+}
 
 void *abb_borrar(abb_t *arbol, const char *clave);
 
